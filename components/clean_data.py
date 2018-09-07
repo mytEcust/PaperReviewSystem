7f8e1357940ac245a5fd5eb4840d9f3cca3c5f25
miyt <yunting.mi@dianrong.com>
2018-09-07 17:13:29 +0800

    预处理文献发表时差数据

--- components/clean_data.py
+++ components/clean_data.py
@@ -12,65 +12,44 @@ import numpy as np
 def clean_out_of_date(format_dir):
     """
     聚类去除参考文献时间与文献发表时间差的时间的异常点
+    参考文献
+    http://scikit-learn.org/stable/auto_examples/neighbors/plot_lof.html#sphx-glr-auto-examples-neighbors-plot-lof-py
+    https://zhuanlan.zhihu.com/p/37753692
     """
-    dates = []
+    print("[START] clean out of date")
     origin_dates = []
-    show_datas = []
     for paper in os.listdir(format_dir):
         with open(format_dir+paper, 'r', encoding="utf-8") as format_data:
             _format_data = format_data.read()
             _format_data = json.loads(_format_data)
             origin_dates += _format_data["out_of_date"]
-            for date in _format_data["out_of_date"]:
-                dates.append([date, 0])
+    # 一维数据转二维
     origin_dates = list(zip(origin_dates, np.zeros_like(origin_dates)))
     clf = LocalOutlierFactor(n_neighbors=20)
     # fit非监督训练
     clf.fit(origin_dates)
-    # scores_pred = clf.negative_outlier_factor_
-    # for i, _d in enumerate(origin_dates):
-    #     show_datas.append([_d, y_pred[i]])
-    # 参考文献
-    # https://zhuanlan.zhihu.com/p/37753692
-    a = clf.kneighbors(origin_dates)[0].max(axis=1)
-    print('离群系数')
-    print(a)
+    # 离群系数
+    outlier = clf.kneighbors(origin_dates)[0].max(axis=1)
+    # 参考文献发表日期 与 文献发表日期的时间差中 最后一个非离群点
+    out_of_date = 0
+    for i, _d in enumerate(origin_dates):
+        if outlier[i] == 0 and _d[0] > out_of_date:
+            out_of_date = _d[0]
+    _clean_out_of_date(format_dir, out_of_date)
+    print("[DONE] clean out of date")
 
-    # http://scikit-learn.org/stable/auto_examples/neighbors/plot_lof.html#sphx-glr-auto-examples-neighbors-plot-lof-py
-    # np.random.seed(42)
 
-    # # Generate train data
-    # X = 0.3 * np.random.randn(100, 2)
-    # # Generate some abnormal novel observations
-    # X_outliers = np.random.uniform(low=-4, high=4, size=(20, 2))
-    # print(X)
-    # print('-------------')
-    # print(X_outliers)
-    # X = np.r_[X + 2, X - 2, X_outliers]
-
-    # # X=dates
-
-    # # fit the model
-    # clf = LocalOutlierFactor(n_neighbors=20)
-    # y_pred = clf.fit_predict(X)
-    # y_pred_outliers = y_pred[200:]
-    # # plot the level sets of the decision function
-    # xx, yy = np.meshgrid(np.linspace(-5, 5, 50), np.linspace(-5, 5, 50))
-    # Z = clf._decision_function(np.c_[xx.ravel(), yy.ravel()])
-    # Z = Z.reshape(xx.shape)
-
-    # plt.title("Local Outlier Factor (LOF)")
-    # plt.contourf(xx, yy, Z, cmap=plt.cm.Blues_r)
-
-    # a = plt.scatter(X[:200, 0], X[:200, 1], c='white',
-    #                 edgecolor='k', s=20)
-    # b = plt.scatter(X[200:, 0], X[200:, 1], c='red',
-    #                 edgecolor='k', s=20)
-    # plt.axis('tight')
-    # plt.xlim((-5, 5))
-    # plt.ylim((-5, 5))
-    # plt.legend([a, b],
-    #         ["normal observations",
-    #             "abnormal observations"],
-    #         loc="upper left")
-    # plt.show()
+def _clean_out_of_date(format_dir, outlier):
+    """
+    清除时间差的异常点
+    """
+    for paper in os.listdir(format_dir):
+        with open(format_dir+paper, 'r+', encoding="utf-8") as format_data:
+            _format_data = format_data.read()
+            _format_data = json.loads(_format_data)
+            out_of_date = _format_data["out_of_date"]
+            out_of_date = list(filter(lambda x: x <= outlier, out_of_date))
+            _format_data["out_of_date"] = out_of_date
+            # 将写入指针指向开头,即覆盖源文件
+            format_data.seek(0, 0)
+            format_data.write(json.dumps(_format_data, ensure_ascii=False))
--- index.py
+++ index.py
@@ -19,14 +19,14 @@ MODEL_DIR = './model/'
 
 if __name__ == '__main__':
     # 初始化工作
-    # pw.init_dir([TXT_DIR, TRAIN_DIR, FORMAT_DIR, MODEL_DIR])
-    # pw.switch_pdf(PAPER_DIR, TXT_DIR)
-    # pw.format_paper(TXT_DIR, FORMAT_DIR)
-    # pw.format_train_data(TXT_DIR, TRAIN_DIR)
+    pw.init_dir([TXT_DIR, TRAIN_DIR, FORMAT_DIR, MODEL_DIR])
+    pw.switch_pdf(PAPER_DIR, TXT_DIR)
+    pw.format_paper(TXT_DIR, FORMAT_DIR)
+    pw.format_train_data(TXT_DIR, TRAIN_DIR)
 
-    # # 段落向量计算相似度
-    # dv.train_datasest(TRAIN_DIR)
-    # dv.run_model(TRAIN_DIR, FORMAT_DIR)
+    # 段落向量计算相似度
+    dv.train_datasest(TRAIN_DIR)
+    dv.run_model(TRAIN_DIR, FORMAT_DIR)
 
     # 数据预处理
     cd.clean_out_of_date(FORMAT_DIR)
