c4d15281c050362b116af9e80e4ebda0d4c69432
miyt <yunting.mi@dianrong.com>
2019-05-13 18:24:49 +0800

    +测试流程

--- src/test.py
+++ src/test.py
@@ -1,32 +1,38 @@
 # -*- coding: utf-8 -*-
 """
-入口函数
+测试函数
 """
 
-__author__ = 'myt'
-
 from configobj import ConfigObj
-
+import sys 
+sys.path.append("..") 
 from components import pre_work as pw
 from components import doc2vec as dv
 from components import clean_data as cd
 from components import one_class_svm as svm
 from components import report as rp
 
-config = ConfigObj('./config/default.conf')
+config = ConfigObj('../config/default.conf')
 
-PAPER_DIR = config['train']['paper_dir']
-TEST_DIR = config['train']['test_dir']
-TXT_DIR = config['train']['txt_dir']
-TRAIN_DIR = config['train']['train_dir']
-FORMAT_DIR = config['train']['format_dir']
-MODEL_DIR = config['train']['model_dir']
+PAPER_DIR = config['ecice']['paper_dir']
+TXT_DIR = config['ecice']['txt_dir']
+TRAIN_DIR = config['ecice']['train_dir']
+FORMAT_DIR = config['ecice']['format_dir']
+MODEL_DIR = config['ecice']['model_dir']
 
-EXEC_DIR = config['predict']['paper_dir']
-EXEC_TXT_DIR = config['predict']['txt_dir']
-EXEC_FORMAT_DIR = config['predict']['format_dir']
-EXEC_TRAIN_DIR = config['predict']['train_dir']
-EXEC_REPORT_DIR = config['predict']['report_dir']
+JOS = {}
+JOS['EXEC_DIR'] = config['jos']['paper_dir']
+JOS['EXEC_TXT_DIR'] = config['jos']['txt_dir']
+JOS['EXEC_FORMAT_DIR'] = config['jos']['format_dir']
+JOS['EXEC_TRAIN_DIR'] = config['jos']['train_dir']
+JOS['EXEC_REPORT_DIR'] = config['jos']['report_dir']
+
+CEAJ = {}
+CEAJ['EXEC_DIR'] = config['ceaj']['paper_dir']
+CEAJ['EXEC_TXT_DIR'] = config['ceaj']['txt_dir']
+CEAJ['EXEC_FORMAT_DIR'] = config['ceaj']['format_dir']
+CEAJ['EXEC_TRAIN_DIR'] = config['ceaj']['train_dir']
+CEAJ['EXEC_REPORT_DIR'] = config['ceaj']['report_dir']
 
 
 def creat_model():
@@ -57,13 +63,20 @@ def creat_model():
     print('--- DONE CREATE MODEL ---')
 
 
-def predict_model():
+def predict_model(predict_data):
     """
     预测数据
     """
     print('--- START PREDICT MODEL ---')
+    EXEC_DIR = predict_data['EXEC_DIR']
+    EXEC_TXT_DIR = predict_data['EXEC_TXT_DIR']
+    EXEC_FORMAT_DIR = predict_data['EXEC_FORMAT_DIR']
+    EXEC_TRAIN_DIR = predict_data['EXEC_TRAIN_DIR']
+    EXEC_REPORT_DIR = predict_data['EXEC_REPORT_DIR']
+
     # 1. clean model data
-    pw.init_dir([EXEC_TXT_DIR, EXEC_FORMAT_DIR, EXEC_TRAIN_DIR])
+    pw.init_dir([EXEC_TXT_DIR, EXEC_FORMAT_DIR,
+                 EXEC_TRAIN_DIR, EXEC_REPORT_DIR])
     # 2. pdf2txt
     pw.switch_pdf(EXEC_DIR, EXEC_TXT_DIR)
     # 3. get basic feature
@@ -87,4 +100,5 @@ def predict_model():
 
 if __name__ == '__main__':
     creat_model()
-    predict_model()
+    predict_model(JOS)
+    predict_model(CEAJ)
